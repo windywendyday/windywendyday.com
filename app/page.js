0d1605d1601230e5f1a9a96d0dcc40e639a4ed31
windywendyday <402089324@qq.com>
2025-05-06 15:59:20 +0800

    fix: 隐去部分文章

--- app/page.js
+++ app/page.js
@@ -8,18 +8,18 @@ import Navigation from "@/app/modules/navigation";
 export default function Home() {
     const articles = [
         { id: 1, title: "们社科女也有春天。。" },
-        { id: 2, title: "前端秋招指北" },
-        { id: 3, title: "八股（困难版）之JS" },
-        { id: 4, title: "八股（困难版）之CSS" },
-        { id: 5, title: "八股（困难版）之React" },
-        { id: 6, title: "八股（困难版）之Vue" },
-        { id: 7, title: "八股（困难版）之Webpack" },
-        { id: 8, title: "八股（困难版）之网络" },
-        { id: 9, title: "八股（困难版）之计算机基础" },
-        { id: 10, title: "八股（困难版）之手写代码和看输出" },
-        { id: 11, title: "八股（困难版）之浏览器" },
-        { id: 12, title: "八股（困难版）之性能优化" },
-        { id: 16, title: "最近从中感受到能量的一些句子" },
+        // { id: 2, title: "前端秋招指北" },
+        // { id: 3, title: "八股（困难版）之JS" },
+        // { id: 4, title: "八股（困难版）之CSS" },
+        // { id: 5, title: "八股（困难版）之React" },
+        // { id: 6, title: "八股（困难版）之Vue" },
+        // { id: 7, title: "八股（困难版）之Webpack" },
+        // { id: 8, title: "八股（困难版）之网络" },
+        // { id: 9, title: "八股（困难版）之计算机基础" },
+        // { id: 10, title: "八股（困难版）之手写代码和看输出" },
+        // { id: 11, title: "八股（困难版）之浏览器" },
+        // { id: 12, title: "八股（困难版）之性能优化" },
+        // { id: 16, title: "最近从中感受到能量的一些句子" },
     ]
     return (
         <div className={styles.app}>
